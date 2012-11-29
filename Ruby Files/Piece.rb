1c31e9c52f923cb3cbc575e792d07dfe497f21a0
Logan <loadams1@umbc.edu>
2012-11-29 13:56:47 -0500

    Project Files

---
+++ Ruby Files/Piece.rb
@@ -0,0 +1,19 @@
+class Piece
+  
+  def initialize(x, y, col)
+    @x, @y, @color = x, y, col;
+  end
+  
+  def getX()
+    @x;
+  end
+  
+  def getY()
+    @y;
+  end
+  
+  def getColor()
+    @color;
+  end
+  
+end
